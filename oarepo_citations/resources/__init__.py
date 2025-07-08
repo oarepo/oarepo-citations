5d7208131525ab4521701b6352aa85cc87f53a07
Martin Čorovčák <martino.coro@gmail.com>
2025-07-08 14:15:18 +0200

    Refactor CSL export handling; update csl_url_args_retriever to use current locale as fallback and add default "text/x-bibliography" Accept value for the rest of citations in Invenio

--- oarepo_citations/resources/__init__.py
+++ oarepo_citations/resources/__init__.py
@@ -1,4 +1,5 @@
 from flask import request
+from invenio_i18n.ext import current_i18n
 from invenio_records_resources.resources.records.headers import etag_headers
 from oarepo_runtime.i18n import lazy_gettext as _
 from oarepo_runtime.resources.responses import ExportableResponseHandler
@@ -10,7 +11,7 @@ from .csl import CSLBibTexSerializer, CSLJSONSerializer, StringCitationSerialize
 def csl_url_args_retriever():
     """Returns the style and locale passed as URL args for CSL export."""
     style = request.args.get("style")
-    locale = request.args.get("locale")
+    locale = request.args.get("locale", current_i18n.locale)
     return style, locale
 
 
@@ -26,7 +27,7 @@ def _bibliography_headers(obj_or_list, code, many=False):
 
 def create_citation_response_handlers(csl_json_schema_cls: Schema):
     """Create citation response handlers based on the provided CSL JSON schema.
-    
+
     :param csl_json_schema_cls: CSL JSON schema class for serialization.
     """
     return {
@@ -39,12 +40,22 @@ def create_citation_response_handlers(csl_json_schema_cls: Schema):
         "text/x-iso-690+plain": ExportableResponseHandler(
             export_code="citation",
             name=_("Citation string"),
-            serializer=StringCitationSerializer(csl_json_schema_cls, url_args_retriever=csl_url_args_retriever),
+            serializer=StringCitationSerializer(
+                csl_json_schema_cls, url_args_retriever=csl_url_args_retriever
+            ),
             headers=_bibliography_headers,
         ),
         "text/x-bibtex+plain": ExportableResponseHandler(
             export_code="bibtex",
             name=_("BibTex"),
             serializer=CSLBibTexSerializer(csl_json_schema_cls),
+        ),
+        "text/x-bibliography": ExportableResponseHandler(
+            export_code="bibliography",
+            name=_("Bibliography"),
+            serializer=StringCitationSerializer(
+                csl_json_schema_cls, url_args_retriever=csl_url_args_retriever
+            ),
+            headers=_bibliography_headers,
         ),
     }
